763ff70465a10586592e22af7964c7554e695d39
DoyubKwon <kdoyub@gmail.com>
2016-10-19 13:40:29 +0900

    custom mybatis & service update
    update

---
+++ 05-소스코드 구현/Client_v2/src/project/jsp/bakeryl/test/service/Custom/InsertCustomServiceItemTest.java
@@ -0,0 +1,45 @@
+package project.jsp.bakeryl.test.service.Custom;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import project.jsp.bakery.dao.MyBatisConnectionFactory;
+import project.jsp.bakery.model.Custom;
+import project.jsp.bakery.service.CustomService;
+import project.jsp.bakery.service.impl.CustomServiceImpl;
+
+public class InsertCustomServiceItemTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Custom custom = new Custom();
+		custom.setCuClassify("크림");
+		custom.setCuName("모카크림");
+		custom.setCuPrice(3000);
+		custom.setCuEditDate("2016-10-11");
+		custom.setCuRegDate("2016-10-11");
+		/** (2) */
+		SqlSession sqlSession = MyBatisConnectionFactory.getSqlSession();
+
+		Logger logger = LogManager.getFormatterLogger(InsertCustomServiceItemTest.class.getName());
+
+		CustomService customService = new CustomServiceImpl(sqlSession, logger);
+
+		/** (3) */
+		try {
+			customService.insertCustomItem(custom);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getLocalizedMessage());
+			return;
+		} finally {
+			sqlSession.close();
+		}
+
+		/** (4) */
+		System.out.println(custom.getId() + "번 데이터 저장됨");
+
+	}
+
+}
