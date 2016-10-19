9d28ed478f9976530a05b4dff157a1dceb07d25e
jotki3 <dlagkswhd@gmail.com>
2016-10-19 17:54:44 +0900

    맵퍼수정 컨트롤러 생성

---
+++ 05-소스코드 구현/Client_v2/src/project/jsp/bakery/controller/bbs/DocumentList.java
@@ -0,0 +1,17 @@
+package project.jsp.bakery.controller.bbs;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DocumentList")
+public class DocumentList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	
+
+}
--- 05-소스코드 구현/Client_v2/src/project/jsp/bakery/service/impl/DocumentServiceImpl.java
+++ 05-소스코드 구현/Client_v2/src/project/jsp/bakery/service/impl/DocumentServiceImpl.java
@@ -8,19 +8,20 @@ import org.apache.logging.log4j.Logger;
 import project.jsp.bakery.model.Document;
 import project.jsp.bakery.service.DocumentService;
 
+
 public class DocumentServiceImpl implements DocumentService {
 
 	/** 처리 결과를 기록할 Log4J객체 생성 */
 	Logger logger;
 	/** MyBatis */
 	SqlSession sqlSession;
+
 	/** 생성자를 통한 객체 생성 */
 	public DocumentServiceImpl(SqlSession sqlSession, Logger logger) {
 		this.sqlSession = sqlSession;
 		this.logger = logger;
 	}
-	
-	
+
 	@Override
 	public void insertDocument(Document document) throws Exception {
 		try {
@@ -38,8 +39,8 @@ public class DocumentServiceImpl implements DocumentService {
 		} finally {
 			sqlSession.commit();
 		}
-	}		
-	
+	}
+
 	@Override
 	public Document selectDocument(Document document) throws Exception {
 		Document result = null;
@@ -94,7 +95,7 @@ public class DocumentServiceImpl implements DocumentService {
 		} finally {
 			sqlSession.commit();
 		}
-		
+
 	}
 
 	@Override
@@ -114,7 +115,89 @@ public class DocumentServiceImpl implements DocumentService {
 		} finally {
 			sqlSession.commit();
 		}
-		
+
+	}
+
+	@Override
+	public Document selectPrevDocument(Document document) throws Exception {
+		Document result = null;
+
+		try {
+			// 이전글이 없는 경우도 있으므로, 리턴값이 null인 경우 예외를 발생하지 않는다.
+			result = sqlSession.selectOne("DocumentMapper.selectPrevDocument", document);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("이전글 조회에 실패했습니다.");
+		}
+
+		return result;
+	}
+
+	@Override
+	public Document selectNextDocument(Document document) throws Exception {
+
+		Document result = null;
+		try {
+			// 다음글이 없는 경우도 있으므로, 리턴값이 null인 경우 예외를 발생하지 않는다.
+			result = sqlSession.selectOne("DocumentMapper.selectNextDocument", document);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("다음글 조회에 실패했습니다.");
+		}
+
+		return result;
+	}
+
+	@Override
+	public int selectDocumentCount(Document document) throws Exception {
+		int result = 0;
+
+		try {
+			// 게시물 수가 0건인 경우도 있으므로,
+			// 결과값이 0인 경우에 대한 예외를 발생시키지 않는다.
+			result = sqlSession.selectOne("DocumentMapper.selectDocumentCount", document);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("게시물 수 조회에 실패했습니다.");
+		}
+
+		return result;
+	}
+
+	@Override
+	public int selectDocumentCountByMemberId(Document document) throws Exception {
+		int result = 0;
+
+		try {
+			// 자신의 게시물이 아닌 경우도 있으므로,
+			// 결과값이 0인 경우에 대한 예외를 발생시키지 않는다.
+			result = sqlSession.selectOne("DocumentMapper.selectDocumentCountByMemberId", document);
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("게시물 수 조회에 실패했습니다.");
+		}
+		return result;
+	}
+
+	@Override
+	public int selectDocumentCountByPw(Document document) throws Exception {
+		int result = 0;
+
+		try {
+			result = sqlSession.selectOne("DocumentMapper.selectDocumentCountByPw", document);
+			// 비밀번호가 일치하는 데이터의 수가 0이라면 비밀번호가 잘못된 것으로 간주한다.
+			if (result < 1) {
+				throw new NullPointerException();
+			}
+		} catch (NullPointerException e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("비밀번호를 확인하세요.");
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage());
+			throw new Exception("게시물 수 조회에 실패했습니다.");
+		}
+
+		return result;
 	}
 
 }
