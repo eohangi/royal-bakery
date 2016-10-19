e733deaaf884d189b898472cf8f2f14ce755a39a
chunsa1615 <chunsa1615@gmail.com>
2016-10-19 11:34:02 +0900

    porduct 작업
    작업중

--- 05-소스코드 구현/Client_v2/src/project/jsp/bakery/controller/product/ProductList.java
+++ 05-소스코드 구현/Client_v2/src/project/jsp/bakery/controller/product/ProductList.java
@@ -7,35 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import project.jsp.helper.BaseController;
+
 /**
  * Servlet implementation class ProductList
  */
-@WebServlet("/ProductList")
-public class ProductList extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProductList() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+@WebServlet("/product/productlist.do")
+public class ProductList extends BaseController {
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public String doRun(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		return null;
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	
 
 }
