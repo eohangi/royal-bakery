05c0c7cf4cafae254306bdca17963847b0cf349c
DoyubKwon <kdoyub@gmail.com>
2016-11-08 12:06:10 +0900

    custom db

---
+++ 07 - erd/20161108_customDB/mydb_custom.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	5.7.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `custom`
+--
+
+DROP TABLE IF EXISTS `custom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `custom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cu_classify` varchar(45) NOT NULL COMMENT '분류\nex) 크림, 단수, 사이즈 ...',
+  `cu_name` varchar(45) NOT NULL COMMENT '옵션이름',
+  `cu_price` int(11) NOT NULL,
+  `cu_edit_date` datetime NOT NULL COMMENT '옵션 수정 날짜\n',
+  `cu_reg_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `custom`
+--
+
+LOCK TABLES `custom` WRITE;
+/*!40000 ALTER TABLE `custom` DISABLE KEYS */;
+INSERT INTO `custom` VALUES (30,'크림','초코크림',3000,'2016-10-24 15:47:07','2016-10-24 15:47:07'),(31,'크림','생크림',5000,'2016-10-24 15:47:20','2016-10-24 15:47:20'),(32,'크기','1호',4000,'2016-10-24 15:49:48','2016-10-24 15:49:48'),(33,'크기','2호',6000,'2016-10-24 15:49:56','2016-10-24 15:49:56'),(34,'단수','1단',3000,'2016-10-24 15:50:16','2016-10-24 15:50:16'),(35,'단수','2단',4000,'2016-10-24 15:50:23','2016-10-24 15:50:23'),(36,'시트','카스테라',4000,'2016-10-24 15:50:33','2016-10-24 15:50:33'),(37,'초','생일초',4000,'2016-10-24 15:51:17','2016-10-24 15:51:17'),(38,'초','장난감초',5000,'2016-10-24 15:51:29','2016-10-24 15:51:29'),(39,'시트','티라미슈',5000,'2016-10-24 15:52:00','2016-10-24 15:52:00'),(40,'크림','초코크림',3000,'2016-10-27 14:21:39','2016-10-27 14:21:39');
+/*!40000 ALTER TABLE `custom` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-08 11:49:28
