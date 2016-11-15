4696e2e0c66a58de2044c4eae48059b2be531750
DoyubKwon <kdoyub@gmail.com>
2016-11-15 13:32:01 +0900

    공용 이메일 추가
    남용하지는 말아주세요 TT

--- 05-소스코드 구현/Client_v2/src/project/jsp/helper/MailHelper.java
+++ 05-소스코드 구현/Client_v2/src/project/jsp/helper/MailHelper.java
@@ -49,7 +49,7 @@ public class MailHelper {
 		Properties p = new Properties();
 
 		// 본인의 Gmail 주소로 계정 설정
-		p.put("mail.smtp.user", "positivenum@gmail.com");
+		p.put("mail.smtp.user", "hdoyub@gmail.com");
 
 		// Google SMTP 서버 정보 설정 
 		p.put("mail.smtp.host", "smtp.gmail.com");
